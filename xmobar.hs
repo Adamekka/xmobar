e0feb06cf471d20c10885e7490fa79ae6f40f50b
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-20 18:27:29 +0100

    network: use arrows

--- xmobar.hs
+++ xmobar.hs
@@ -37,7 +37,7 @@ Config
         Run Com "uname" ["-s", "-r"] "" 36000,
         Run Date "\xf133  %a %b %d %Y - (%H:%M) \xf017" "date" 10,
         Run Battery ["-t", "<acstatus>: <left>% - <timeleft>h", "--", "-O", "AC", "-o", "BAT", "-l", "red", "-h", "green"] 10,
-        Run DynNetwork ["-t", "<dev>: up:<tx>kB/s down:<rx>kB/s", "-L", "1000", "-H", "1000000", "-h", "red"] 20
+        Run DynNetwork ["-t", "<dev>: \xf55c <tx>kB/s \xf544 <rx>kB/s", "-L", "1000", "-H", "1000000", "-h", "red"] 20
       ],
     sepChar = "%",
     alignSep = "}{",
